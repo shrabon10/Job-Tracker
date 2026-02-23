9608bab96edde6f0d3d8a623ceb154c3f5a752e2
shrabon10 <omarfaruksrabon@gmail.com>
2026-02-24 02:11:00 +0600

    Succesfully Runed All section and features

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
-## Answers to Questions
+## Answers 
 
 ### 1. What is the difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll?
+=> getElementById() selects a single element using its unique ID and returns one Element object.
+=> getElementsByClassName() selects multiple elements using a class name and returns a live HTMLCollection that     updates automatically when the DOM changes.
+=>querySelector() selects the first element that matches any CSS selector,
+=>querySelectorAll() selects all matching elements and returns a static NodeList that does not update automatically.
+
 
 ### 2. How do you create and insert a new element into the DOM?
 
+=>Create a new element using document.createElement() and insert it into the DOM using methods like appendChild() or append().
+exm:-const ul = document.querySelector("ul"); const li = document.createElement("li"); li.textContent = "Item 1"; ul.prepend(li);
+
 ### 3. What is Event Bubbling? And how does it work?
+=>Event Bubbling is a process where an event starts from the target element and propagates upward through its parent elements in the DOM hierarchy.
+When an event (like a click) occurs on a child element, it first runs on that element, then moves to its parent, then grandparent, and continues up to the document object. This allows parent elements to handle events triggered by their children.
 
 ### 4. What is Event Delegation in JavaScript? Why is it useful?
+=>Event Delegation is a technique in JavaScript where a single event listener is attached to a parent element instead of multiple child elements. It works by using event bubbling, where the event moves from the child to the parent. The parent can detect which child triggered the event using event.target.
+It is useful because it improves performance by reducing the number of event listeners, saves memory, and automatically works for dynamically added elements. This makes code cleaner and more efficient.
+
 
 ### 5. What is the difference between preventDefault() and stopPropagation() methods?
+=> preventDefault() stops the browser’s default action for an event, such as preventing a form from submitting or a link from navigating to another page.
+=>stopPropagation() stops the event from bubbling up the DOM tree, meaning parent elements will not receive the event.
 
+=>In short, preventDefault() controls the browser’s behavior, while stopPropagation() controls event flow in the DOM.
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> fe6c0e4a6b06969d9668d5f774e485a3ca6d6a97
--- scripts/jobs.js
+++ scripts/jobs.js
@@ -1,32 +1,50 @@
-// main section
+// main section 
 const mainContainer = document.getElementById("main");
-// job  access 
+
+// job count 
 const totalJOb = document.getElementById("total");
 const interviewJOb = document.getElementById("interview");
 const rejectedJOb = document.getElementById("rejected");
 
-// button access
-const allFilterBtn = document.getElementById("all-filter-btn");
-const interviewFilterBtn = document.getElementById("interview-filter-btn");
-const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+// available job 
+const availableJobCount = document.getElementById("available-job-total");
+let availableInterViewCount = document.getElementById("interview-available");
+let availableRejectedCount = document.getElementById("rejected-available");
 
-// job length 
+// job length required 
 const allCardSection = document.getElementById("all-card");
 const interviewFilterSection = document.getElementById("interview-card");
 const rejectedFilterSection = document.getElementById("rejected-card");
 
+// filter job 
+const allFilterBtn = document.getElementById("all-filter-btn");
+const interviewFilterBtn = document.getElementById("interview-filter-btn");
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+// all card section access
+const allCard = document.querySelectorAll("#all-card .card");
+
+
+// Array create 
 let interviewList = [];
 let rejectedList = [];
+let allCardList = [];
 
-// access length job count
+
+//length job count
 function calculateJobCount() {
-    totalJOb.innerText = allCardSection.children.length;
+
+    totalJOb.innerText = allCardList.length;
     interviewJOb.innerText = interviewList.length;
     rejectedJOb.innerText = rejectedList.length;
+
+    availableJobCount.innerText = allCardList.length;
+    availableInterViewCount.innerText = interviewList.length;
+    availableRejectedCount.innerText = rejectedList.length;
 };
 
 
-// filter  job button 
+// filter  job button toggle 
 function showFilterBtn(id) {
 
     allFilterBtn.classList.remove("bg-[#3b82f6]", "text-white");
@@ -40,25 +58,45 @@ function showFilterBtn(id) {
         interviewFilterSection.classList.remove("hidden");
         allCardSection.classList.add("hidden");
         rejectedFilterSection.classList.add("hidden");
+
+        // available jobs count
+        if (interviewList.length > 0) {
+            availableInterViewCount.parentNode.classList.remove("hidden");
+        } else {
+            availableInterViewCount.parentNode.classList.add("hidden");
+        }
+        availableRejectedCount.parentNode.classList.add("hidden");
+
     } else if (id === "all-filter-btn") {
         allCardSection.classList.remove("hidden");
         interviewFilterSection.classList.add("hidden");
         rejectedFilterSection.classList.add("hidden");
 
+        // available jobs count
+        availableInterViewCount.parentNode.classList.add("hidden");
+        availableRejectedCount.parentNode.classList.add("hidden");
+
     } else if (id === "rejected-filter-btn") {
         rejectedFilterSection.classList.remove("hidden");
         allCardSection.classList.add("hidden");
         interviewFilterSection.classList.add("hidden");
+
+        // available jobs count
+        if (rejectedList.length > 0) {
+            availableRejectedCount.parentNode.classList.remove("hidden");
+        } else {
+            availableRejectedCount.parentNode.classList.add("hidden");
+        }
+        availableInterViewCount.parentNode.classList.add("hidden");
+
     }
 
 };
-
-
 function renderAll() {
     showRenderInterview();
     showRenderReject();
 }
-
+// not available jobs
 function showEmptyCard(section) {
 
     section.innerHTML = "";
@@ -68,7 +106,7 @@ function showEmptyCard(section) {
     div.innerHTML = `
            <div class="flex justify-center items-center mx-auto py-17">
             <div>
-                <img class="mx-auto mb-3" src="images/jobs.png" alt="">
+                <img class="mx-auto mb-3" src="./jobs.png" alt="">
                 <h5 class="font-semibold text-2xl text-center">No jobs available</h5>
                 <p class="font-medium opacity-45 text-center">Check back soon for new job opportunities</p>
             </div>
@@ -77,7 +115,9 @@ function showEmptyCard(section) {
     section.appendChild(div);
 }
 
-// interview section
+if (interviewList.length === 0) {
+    showEmptyCard(interviewFilterSection);
+}
 function showRenderInterview() {
 
     interviewFilterSection.innerHTML = "";
@@ -88,44 +128,56 @@ function showRenderInterview() {
     }
     for (let interView of interviewList) {
         let div = document.createElement("div");
-        div.className = "card flex justify-between p-5 shadow bg-white rounded-xs";
+        div.className = "card flex justify-between p-5 shadow bg-white rounded-xs border-l-4 border-green-500 shadow-green-500/90";
         div.innerHTML = `
-               <div class="space-y-4">
+         <div class="space-y-4 w-full">
 
-                    <div>
-                        <p class="company-name text-xl font-semibold mb-2">${interView.companyName}</p>
-                        <p class="skill-name opacity-50">${interView.skillName}</p>
-                    </div>
+            <div class="flex justify-between ">
 
-                    <div class="flex gap-3 items-center">
-                        <p class="job-location opacity-50">${interView.jobLocation}</p>
+                        <div>
+                            <p class="company-name text-xl font-semibold mb-2">${interView.companyName}</p>
+                            <p class="skill-name opacity-50 ">${interView.skillName}
+                            </p>
+                        </div>
 
-                    </div>
-
-                    <p class="status p-2 shadow inline-block bg-[#eef4ff]">${interView.status}</p>
-                    <p class="notes opacity-60">${interView.notes}</p>
-
-                    <div class="flex gap-5">
-
-                        <button
-                            class="interview-btn border-2 border-green-500 text-green-500 p-2 px-4 rounded font-semibold">Interview</button>
-
-                        <button
-                            class="rejected-btn border-2 border-red-500 text-red-500 p-2 px-4 rounded font-semibold">Rejected</button>
+                        <div>
+                            <button class="delete-btn text-red-600 px-3 py-1 rounded font-extrabold text-3xl">
+                                <span
+                                    class="p-2 shadow  border-red-700 hover:bg-red-100 hover:border transition-all duration-600 rounded-full cursor-pointer"><i
+                                        class="fa-regular fa-trash-can"></i></span>
+                            </button>
+                        </div>
 
                     </div>
-                </div>
+
+            <div class="flex gap-3 items-center">
+                <p class="job-location opacity-50">${interView.jobLocation}</p>
+
+            </div>
+
+            <p
+                class="status p-2 px-4 shadow font-semibold w-fit border-2 rounded-[7px] bg-green-200 text-green-700 border-green-500 cursor-pointer">
+                ${interView.status}</p>
+            <p class="notes opacity-60">${interView.notes}</p>
+
+            <div class="flex gap-5">
+                <button
+                    class="interview-btn border-2 p-1.5 border-green-500 text-green-600  px-4 rounded font-semibold hover:bg-green-100 transition-all duration-300 cursor-pointer">INTERVIEW</button>
+
+                <button
+                    class="rejected-btn border-2 border-red-500 text-red-600 p-1.5 px-4 rounded font-semibold hover:bg-red-100 transition-all duration-300 cursor-pointer">REJECTED</button>
+            </div>
+        </div>
     
-              `;
-
+        `;
         interviewFilterSection.appendChild(div);
+    };
 
-    }
-
-};
-
-
+}
 // rejected section
+if (rejectedList.length === 0) {
+    showEmptyCard(rejectedFilterSection);
+}
 function showRenderReject() {
 
     rejectedFilterSection.innerHTML = "";
@@ -138,13 +190,26 @@ function showRenderReject() {
     for (let reject of rejectedList) {
 
         let div = document.createElement("div");
-        div.className = "card flex justify-between p-5 shadow bg-white rounded-xs";
+        div.className = "card flex justify-between p-5 shadow bg-white rounded-xs border-l-4 border-red-500 shadow-red-500/90";
         div.innerHTML = `
-               <div class="space-y-4">
+               <div class="space-y-4 w-full">
+               
+                    <div class="flex justify-between ">
 
-                    <div>
-                        <p class="company-name text-xl font-semibold mb-2">${reject.companyName}</p>
-                        <p class="skill-name opacity-50">${reject.skillName}</p>
+                        <div>
+                            <p class="company-name text-xl font-semibold mb-2">${reject.companyName}</p>
+                            <p class="skill-name opacity-50 ">${reject.skillName}
+                            </p>
+                        </div>
+
+                        <div>
+                            <button class="delete-btn text-red-600 px-3 py-1 rounded font-extrabold text-3xl">
+                                <span
+                                    class="p-2 shadow  border-red-700 hover:bg-red-100 hover:border transition-all duration-600 rounded-full cursor-pointer"><i
+                                        class="fa-regular fa-trash-can"></i></span>
+                            </button>
+                        </div>
+
                     </div>
 
                     <div class="flex gap-3 items-center">
@@ -152,20 +217,22 @@ function showRenderReject() {
 
                     </div>
 
-                    <p class="status p-2 shadow inline-block bg-[#eef4ff]">${reject.status}</p>
+                     <p class="status p-2 px-4 shadow font-semibold w-fit border-2 rounded-[7px] bg-red-200 text-red-700 border-red-500 cursor-pointer ">${reject.status}</p>
                     <p class="notes opacity-60">${reject.notes}</p>
 
                     <div class="flex gap-5">
 
-                        <button
-                            class="interview-btn border-2 border-green-500 text-green-500 p-2 px-4 rounded font-semibold">Interview</button>
+                      <button
+                            class="interview-btn border-2 p-1.5 border-green-500 text-green-600  px-4 rounded font-semibold hover:bg-green-100 transition-all duration-300 cursor-pointer">INTERVIEW</button>
 
                         <button
-                            class="rejected-btn border-2 border-red-500 text-red-500 p-2 px-4 rounded font-semibold">Rejected</button>
+                            class="rejected-btn border-2 border-red-500 text-red-600 p-1.5 px-4 rounded font-semibold hover:bg-red-100 transition-all duration-300 cursor-pointer">REJECTED</button>
 
                     </div>
+                   
+             
                 </div>
-    
+              
               `;
 
         rejectedFilterSection.appendChild(div);
@@ -173,18 +240,38 @@ function showRenderReject() {
     }
 
 }
-
+// mainContainer
 mainContainer.addEventListener("click", function (event) {
 
-    if (event.target.classList.contains("interview-btn")) {
+
+    if (event.target.closest(".delete-btn")) {
+        let parentNode = event.target.closest(".card");
+        let companyName = parentNode.querySelector(".company-name").innerText;
+
+        allCardList = allCardList.filter(item => item.companyName !== companyName);
+
+        interviewList = interviewList.filter(item => item.companyName !== companyName);
+
+        rejectedList = rejectedList.filter(item => item.companyName !== companyName);
+
+        parentNode.remove();
+        calculateJobCount();
+        renderAll();
+
+        if (allCardList.length === 0) {
+            showEmptyCard(allCardSection);
+        }
+
+
+    } else if (event.target.classList.contains("interview-btn")) {
         let parentNode = event.target.closest(".card");
         let companyName = parentNode.querySelector(".company-name").innerText;
         let skillName = parentNode.querySelector(".skill-name").innerText;
         let jobLocation = parentNode.querySelector(".job-location").innerText;
         let notes = parentNode.querySelector(".notes").innerText;
         let statusEle = parentNode.querySelector(".status");
-        statusEle.innerText = "Interview";
-        let status = "Interview";
+        statusEle.innerText = "INTERVIEW";
+        let status = "INTERVIEW";
 
         const cardInfo = {
             companyName,
@@ -200,10 +287,18 @@ mainContainer.addEventListener("click", function (event) {
             interviewList.push(cardInfo);
         }
 
-        rejectedList = rejectedList.filter(item => item.companyName !== cardInfo.companyName)
+        rejectedList = rejectedList.filter(item => item.companyName !== cardInfo.companyName);
+        
+        statusEle.classList.remove("hidden");
+        statusEle.classList.remove("bg-red-200", "text-red-700", "border-red-500");
+        statusEle.classList.add("bg-green-200", "text-green-700", "border-2", "border-green-500");
+
+        parentNode.classList.remove("rounded-lg", "border-l-4", "border-red-500", "shadow-red-500/90");
+        parentNode.classList.add("rounded-lg", "border-l-4", "border-green-500", "shadow", "shadow-green-500/90");
 
         calculateJobCount();
         renderAll();
+
     }
 
     else if (event.target.classList.contains("rejected-btn")) {
@@ -214,8 +309,8 @@ mainContainer.addEventListener("click", function (event) {
         let jobLocation = parentNode.querySelector(".job-location").innerText;
         let notes = parentNode.querySelector(".notes").innerText;
         let statusEle = parentNode.querySelector(".status");
-        statusEle.innerText = "Rejected";
-        let status = "Rejected";
+        statusEle.innerText = "REJECTED";
+        let status = "REJECTED";
 
         const cardInfo = {
             companyName,
@@ -232,15 +327,37 @@ mainContainer.addEventListener("click", function (event) {
         }
 
         interviewList = interviewList.filter(item => item.companyName !== cardInfo.companyName);
+        statusEle.classList.remove("hidden");
+        statusEle.classList.remove("bg-green-200", "text-green-700", "border-green-500");
+        statusEle.classList.add("bg-red-200", "text-red-700", "border-2", "border-red-500");
+
+        // main card 
+        parentNode.classList.remove("rounded-lg", "border-l-4", "border-green-500", "shadow-green-500/90");
+        parentNode.classList.add("rounded-lg", "border-l-4", "border-red-500", "shadow", "shadow-red-500/90");
 
         calculateJobCount();
         renderAll();
+
     }
 
-
 });
-
 document.addEventListener("DOMContentLoaded", function () {
-    calculateJobCount();
     showFilterBtn("all-filter-btn");
+    for (let card of allCard) {
+        let companyName = card.querySelector(".company-name").innerText;
+        let skillName = card.querySelector(".skill-name").innerText;
+        let jobLocation = card.querySelector(".job-location").innerText;
+        let status = card.querySelector(".status").innerText;
+        let notes = card.querySelector(".notes").innerText;
+
+        let cardObject = {
+            companyName,
+            skillName,
+            jobLocation,
+            status,
+            notes
+        }
+        allCardList.push(cardObject);
+    }
+    calculateJobCount();
 });
